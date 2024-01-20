52b12bb22976a2c975870a9a5c299866dd8adc22
Ademola Fadumo <demolafadumo@gmail.com>
2024-01-20 03:01:46 +0100

    Initial Commit

---
+++ GamesExplorer/GamesExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesExplorerApp.swift
+//  GamesExplorer
+//
+//  Created by Ademola Fadumo on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GamesExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
